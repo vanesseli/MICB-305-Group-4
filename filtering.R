d48899aee0be3daa955fb10d93e31f02d6da2295
Vanesse Li <vanesselicw@gmail.com>
2026-04-11 14:20:17 -0700

    R script for data filtering and filtered dataset in excel

---
+++ filtering.R
@@ -0,0 +1,80 @@
+library(tidyverse)
+library(phyloseq)
+library(readxl)
+library(vegan)
+library(ggpubr)
+library(microbiome)
+library(indicspecies)
+library(writexl)
+library(ANCOMBC)
+library(dplyr)
+
+#loading the data
+metadata_edited = read_excel('depression_metadata_manuscript.xlsx')
+#572
+
+metadata_clean_edited <- metadata_edited |>
+  filter(str_detect(library_name, "blank", negate = TRUE))|>
+  filter(!is.na(library_name))
+
+metadata_clean_edited <- metadata_clean_edited %>%
+  mutate(antidepressant_on_off = recode(as.character(antidepressant_on_off), "0" = "Off","1" = "On")) 
+
+metadata_clean_edited <- metadata_clean_edited|>
+  filter(str_detect(name_sample, "blank", negate = TRUE))|>
+  filter(!is.na(name_sample))
+
+#control for substance use (paper did)
+metadata_clean_edited <- metadata_clean_edited|>  
+  filter(str_detect(current_any_substance_dx, "YES", negate = TRUE))
+#532 samples
+
+#checking for duplicates (all of them should say false)
+any(duplicated(metadata_clean_edited$name_sample))
+any(duplicated(metadata_clean_edited$sample_name))
+any(duplicated(metadata_clean_edited$BioSample))
+any(duplicated(metadata_clean_edited$Sample_name))
+any(duplicated(metadata_clean_edited$host_subject_id))
+
+#if one of them return with ‘TRUE’, check which samples were duplicated with this code
+#metadata_clean_edited$host_subject_id[duplicated(metadata_clean_edited$host_subject_id)]
+
+#only select the columns I care about since the dataset is so wide
+metadata_clean_small <- metadata_clean_edited |>
+  select(hiv_status_clean, antidepressant_on_off, hcv, bdi_group, host_subject_id)
+#532
+
+sum(metadata_clean_small$bdi_group == "NA")
+#151 samples have NA for bdi group
+
+metadata_clean_small <- metadata_clean_small|>
+  filter(bdi_group != "NA")
+#381
+
+#want to see how many NAs there are for antidepressant column (6)
+metadata_clean_small <- metadata_clean_small|>
+  filter(antidepressant_on_off != "NA")
+#375
+
+#people with HIV, no HCV, depressed, use antidepressants
+hiv_depressed_antidepressant <- metadata_clean_small|>
+  filter(hiv_status_clean == 'HIV+')|>
+  filter(hcv == 'NO')|>
+  filter(bdi_group != 'minimal')|>
+  filter(antidepressant_on_off == "On")
+#39 people
+
+#people with HIV, no HCV, depressed, no antidepressants
+hiv_depressed_no_anti <- metadata_clean_small|>
+  filter(hiv_status_clean == 'HIV+')|>
+  filter(hcv == 'NO')|>
+  filter(bdi_group != 'minimal')|>
+  filter(antidepressant_on_off == "Off")
+#24 people in total
+
+#total number of people who has HIV and depression (and no HCV)
+total_people <- metadata_clean_small|>
+  filter(hiv_status_clean == 'HIV+')|>
+  filter(hcv == 'NO')|>
+  filter(bdi_group != 'minimal')
+#63 people in total
