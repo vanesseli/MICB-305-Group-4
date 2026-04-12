3189fd6d739227233130d01f89d823f143065ded
Vanesse Li <vanesselicw@gmail.com>
2026-04-12 12:35:13 -0700

    updated indicator plot

--- indicator_species/indicator_species.R
+++ indicator_species/indicator_species.R
@@ -10,52 +10,11 @@ library(ANCOMBC)
 library(dplyr)
 
 #loading data
-metadata_edited = read_excel('filtering/depression_metadata_manuscript.xlsx')
-#572
+metadata = read.csv('filtering/metadata_filtered.csv')
+#61
 
-metadata_edited <- as.data.frame(metadata_edited)
-rownames(metadata_edited) <- metadata_edited$sample_id
-
-metadata_clean_edited <- metadata_edited |>
-  filter(str_detect(library_name, "blank", negate = TRUE))|>
-  filter(!is.na(library_name))
-
-metadata_clean_edited <- metadata_clean_edited %>%
-  mutate(antidepressant_on_off = recode(as.character(antidepressant_on_off), "0" = "Off","1" = "On")) 
-
-#control for substance use (paper did)
-metadata_clean_edited <- metadata_clean_edited|>  
-  filter(current_any_substance_dx == "NO")
-#427 samples
-
-metadata_clean_edited <- metadata_clean_edited|>
-  filter(bdi_group != "NA")
-#364
-
-##subsetting
-#people with HIV, no HCV, depressed, use antidepressants
-hiv_depressed_antidepressant <- metadata_clean_edited|>
-  filter(hiv_status_clean == 'HIV+')|>
-  filter(hcv == 'NO')|>
-  filter(bdi_group != 'minimal')|>
-  filter(antidepressant_on_off == "On")
-#38 people
-
-#people with HIV, no HCV, depressed, no antidepressants
-hiv_depressed_no_anti <- metadata_clean_edited|>
-  filter(hiv_status_clean == 'HIV+')|>
-  filter(hcv == 'NO')|>
-  filter(bdi_group != 'minimal')|>
-  filter(antidepressant_on_off == "Off")
-#23 people in total
-
-View(metadata_clean_edited)
-#total number of people who has HIV and depression (and no HCV)
-total_people <- metadata_clean_edited|>
-  filter(hiv_status_clean == 'HIV+')|>
-  filter(hcv == 'NO')|>
-  filter(bdi_group != 'minimal')
-#61 people in total
+metadata <- as.data.frame(metadata)
+rownames(metadata) <- metadata$sample_id
 
 #indicator species analysis
 ps = readRDS("project_analyses/my_phyloseq_object.rds")|>
@@ -93,6 +52,16 @@ tax_table(ps)[grep("Anaerovibrio", tax_table(ps)[, "Genus"]), ]
 #what are the taxonomic ranks of g__Clostridia_UCG-014 
 tax_table(ps)[grep("Clostridia_UCG-014", tax_table(ps)[, "Genus"]), ]
 
+#what are the taxonomic ranks of   g__Fusobacterium
+tax_table(ps)[grep("Fusobacterium", tax_table(ps)[, "Genus"]), ]
+#what are the taxonomic ranks of  g__Rikenellaceae_RC9_gut_group
+tax_table(ps)[grep("Rikenellaceae_RC9_gut_group", tax_table(ps)[, "Genus"]), ]
+#what are the taxonomic ranks of  g__Muribaculaceae
+tax_table(ps)[grep("Muribaculaceae", tax_table(ps)[, "Genus"]), ]
+#what are the taxonomic ranks of  g__Geobacillus
+tax_table(ps)[grep("Geobacillus", tax_table(ps)[, "Genus"]), ]
+
+
 #plotting
 phyla_to_plot = indval_table%>%
   filter(s.off ==1 & s.on==0)%>%
@@ -111,5 +80,7 @@ plot_filter <- df_of_taxa%>%
 library(scales)
 indicator_species_plot <- plot_filter +
   scale_y_continuous(trans = pseudo_log_trans())+ scale_fill_manual(values = c("off" = "#4876FF","on"  = "#FF82AB" )) 
+
+indicator_species_plot
 
 ggsave("indicator_species/indicator_species.png", plot = indicator_species_plot, width = 10, height = 10, limitsize = FALSE)
