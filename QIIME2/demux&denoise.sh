12863220b377c4ea73ccf7ab6496eec49861d457
Katelyn <159168952+kmwang16@users.noreply.github.com>
2026-04-17 12:42:14 -0700

    Update demux&denoise.sh

--- QIIME2/demux&denoise.sh
+++ QIIME2/demux&denoise.sh
@@ -1,14 +1,11 @@
 ## INITIAL STEPS: DENMULTIPLEX AND DENOISE ##
-# cd: local computer (Desktop/MICB_305/Group_Project)
-# import dataset from computer onto server 
-scp Desktop/MICB_305/Group_Project/metadata_filtered.csv root@10.34.36.91:/work/manuscript
 
-# cd: remote server (/work/manuscript)
+# cd: remote server (/work/assignment_1)
 # #importing manifest file as well as demultiplexing
 qiime tools import \
   --type "SampleData[SequencesWithQuality]" \
   --input-format SingleEndFastqManifestPhred33V2 \
-  --input-path /datasets/project_2/depression/depression_manifest.tsv \
+  --input-path /datasets/assigment_1/depression/depression_manifest.tsv \
   --output-path ./demux_seqs.qza
  
 # converting demultiplexed file to visualization format
@@ -18,11 +15,11 @@ qiime demux summarize \
 
 # copying visualization file to local computer 
 # cd: local computer (Desktop/MICB_305/Group_Project)
-scp root@10.34.36.91:/work/manuscript/demux.qzv .
+scp root@10.34.36.91:/work/assignment_1/demux.qzv .
 
 # denoising
 # deterine ASVs
-# cd: remote server (/work/manuscript)
+# cd: remote server (/work/assignment_1)
 qiime dada2 denoise-single \
   --i-demultiplexed-seqs demux_seqs.qza \
   --p-trim-left 0 \
@@ -39,8 +36,17 @@ qiime metadata tabulate \
 qiime feature-table summarize \
   --i-table table.qza \
   --o-visualization table.qzv \
-  --m-sample-metadata-file metadata_filtered.tsv
+  --m-sample-metadata-file /datasets/project_2/depression/depression_metadata_full.txt****
 
 qiime feature-table tabulate-seqs \
   --i-data rep-seqs.qza \
   --o-visualization rep-seqs.qzv
+
+# generate a tree for phylogenetic diversity analyses
+qiime phylogeny align-to-tree-mafft-fasttree \
+  --i-sequences rep-seqs.qza \
+  --o-alignment aligned-rep-seqs.qza \
+  --o-masked-alignment masked-aligned-rep-seqs.qza \
+  --o-tree unrooted-tree.qza \
+  --o-rooted-tree rooted-tree.qza 
+
