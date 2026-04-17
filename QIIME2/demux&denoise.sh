0c13fb9ed989557637e4b3867badc0134a4602f6
Katelyn <159168952+kmwang16@users.noreply.github.com>
2026-04-17 12:43:08 -0700

    Update demux&denoise.sh

--- QIIME2/demux&denoise.sh
+++ QIIME2/demux&denoise.sh
@@ -41,12 +41,3 @@ qiime feature-table summarize \
 qiime feature-table tabulate-seqs \
   --i-data rep-seqs.qza \
   --o-visualization rep-seqs.qzv
-
-# generate a tree for phylogenetic diversity analyses
-qiime phylogeny align-to-tree-mafft-fasttree \
-  --i-sequences rep-seqs.qza \
-  --o-alignment aligned-rep-seqs.qza \
-  --o-masked-alignment masked-aligned-rep-seqs.qza \
-  --o-tree unrooted-tree.qza \
-  --o-rooted-tree rooted-tree.qza 
-
